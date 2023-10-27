2b260b9ad56bde526fe7654310b85ecf90a8963d
Methupa Perera <methupapereradev@gmail.com>
2023-10-27 11:08:50 +0530

    Changes made to rearrange the code.

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const fs = require("fs");
-const express = require("express");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
-
-const app = express();
-const PORT = 4000;
-
-const template = fs.readFileSync("../template.html", "utf8");
-
-const replacer = (file, name, email, message) => {
-    file = file.replace("{{name}}", name);
-    file = file.replace("{{email}}", email);
-    file = file.replace("{{message}}", message);
-
-    return file;
-};
-
-
-app.use(cors());
-app.use(express.json());
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -28,48 +8,34 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-app.get("/", (req, res) => {
-    res.status(200).send("<h1>Email Sender!</h1>");
+const mailOptions = {
+    from: "methupapereradev@gmail.com",
+    to: "methupapereradev@gmail.com",
+    subject: "New Contact Message.",
+    html: replacer(template, name, email, message),
+};
+
+// Send the email
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error(error);
+        res.status(500).send(
+            JSON.stringify({
+                status: "failed",
+                data: {
+                    message: "Failed to send email !",
+                },
+            })
+        );
+    } else {
+        console.log("Email sent: " + info.response);
+        res.status(200).send(
+            JSON.stringify({
+                status: "success",
+                data: {
+                    message: "Email sent !",
+                },
+            })
+        );
+    }
 });
-
-app.post("/send-mail", (req, res) => {
-    const { name, email, message } = req.body;
-
-    const mailOptions = {
-        from: "methupapereradev@gmail.com",
-        to: "methupapereradev@gmail.com",
-        subject: "New Contact Message.",
-        html: replacer(template, name, email, message),
-    };
-
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send(
-                JSON.stringify({
-                    status: "failed",
-                    data: {
-                        message: "Failed to send email !",
-                    },
-                })
-            );
-        } else {
-            console.log("Email sent: " + info.response);
-            res.status(200).send(
-                JSON.stringify({
-                    status: "success",
-                    data: {
-                        message: "Email sent !",
-                    },
-                })
-            );
-        }
-    });
-});
-
-app.listen(PORT, (req, res) => {
-    console.log("Server is running on http://127.0.0.1:" + PORT);
-});
-
-module.exports = app;
